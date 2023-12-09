579d54afc71378c5384378ab5738ac11b33206b9
Liz Garcia <lizgarova16@gmail.com>
2023-12-09 05:38:46 -0500

    modularizing skip times

--- sjf/sjf.go
+++ sjf/sjf.go
@@ -1,38 +1,40 @@
 package sjf
 
+import (
+	"fmt";
+)
+
+/*
+Shortest job first scheduling policy: selects the waiting process with smallest execution time to execute next,
+regardless of an incoming job having a shorter estimated execution time than the job currently running.
+*/
+
 type Process struct {
-	id          int
-	arrivalTime int //
-	burstTime   int // estimate of how long job takes
+	id          int // identifier for each process
+	arrivalTime int // time when process is included to run
+	burstTime   int // estimated amount of time for job to complete
 	// execution time = arrivaltime + bursttime
-	waitingTime int
+	waitingTime int // how long each process waits from the moment they arrive until it is completed 
 }
 
 type SJF struct {
-	queue                  []Process //priority queue sorting based on burst time
+	queue                  []Process // priority queue sorting based on burst time
 	remainingTime          int       // time left for current running process
-	processId              int       // current process
-	totalWaitingTime       int
-	totalProcessesExecuted int
-	processes              map[int]*Process
+	processId              int       // current process identifier
+	totalWaitingTime       int 		// waiting time across all processes
+	totalProcessesExecuted int 		// total number of processes executed 
+	processes              map[int]*Process // key: process id, value: pointer to process 
 }
 
 func NewSJF() *SJF {
 	sjf := new(SJF)
 	sjf.queue = make([]Process, 0)
-	sjf.processId = -1
+	sjf.processId = -1 
 	sjf.processes = make(map[int]*Process)
 	return sjf
 }
 
 func (sjf *SJF) run(process *Process, currentTime int) {
-	// sort all processes according to arrival time
-	// arrival time is end of scheduler len(scheduler) - 1
-	// select process that has min arrival time and min burst time
-	//add to queue
-
-	//queue = append(queue, Process{id = processId, burstTime = burstTime, arrivalTime = arrivalTime})
-
 	// index 0 = smallest burst time
 	// 0 1 2 3        2
 	// 0 2 4 5 -- 0 2 2 4 5
@@ -41,25 +43,29 @@ func (sjf *SJF) run(process *Process, currentTime int) {
 
 	if process != nil {
 		//process := Process{id = processId, burstTime = burstTime, arrivalTime=arrivalTime}
-
+		
+		// insert new process into hashmap
 		sjf.processes[process.id] = process
 
 		found := false
+		// Decide where to place new process in the queue (which is the "waiting line")
 		for i := 0; i < len(sjf.queue); i++ {
 			if sjf.queue[i].burstTime > process.burstTime {
 				found = true
+				// append new process to front of queue because it has smallest burstime
 				if i == 0 {
 					sjf.queue = append([]Process{*process}, sjf.queue[:]...)
-
-				} else {
+				
+				// burstime of current process is less than the ith process, 
+				// so place new process before it 
+				} else { 
 					sjf.queue = append(sjf.queue[:i], sjf.queue[i-1:]...)
 					sjf.queue[i] = *process
 				}
 				break
 			}
-
 		}
-		// in the case that queue is empty
+		// in the case that queue is empty, append new process
 		if !found {
 			sjf.queue = append(sjf.queue, *process)
 		}
@@ -67,16 +73,17 @@ func (sjf *SJF) run(process *Process, currentTime int) {
 
 	// (0,1), (1,1), (1,2)
 
+	// check if current process finished to decide which job runs next (if any)
 	if sjf.remainingTime == 0 {
 		if len(sjf.queue) == 0 {
-			sjf.processId = -1
+			sjf.processId = -2 // no more processes to execute
 		} else {
-			nextJob := sjf.queue[0]
-			sjf.queue = sjf.queue[1:] //pop from queue
-			sjf.processId = nextJob.id
-			sjf.remainingTime = nextJob.burstTime
-			waitingTime := currentTime - nextJob.arrivalTime // nextJob.arrivalTime = start time of next job
-			sjf.processes[sjf.processId].waitingTime = waitingTime
+			nextJob := sjf.queue[0] // take next job from the front of queue
+			sjf.queue = sjf.queue[1:] // pop from queue
+			waitingTime := currentTime - nextJob.arrivalTime 
+			nextJob.waitingTime = waitingTime
+			sjf.processId = nextJob.id // set nextJob to be the current process now
+			sjf.remainingTime = nextJob.burstTime // set remaining time to burstTime of new process to execute
 
 			sjf.totalWaitingTime += waitingTime
 			sjf.totalProcessesExecuted += 1
@@ -85,9 +92,6 @@ func (sjf *SJF) run(process *Process, currentTime int) {
 	}
 
 	sjf.remainingTime--
-
-	//
-
 }
 
 // get process you want to run at this time
@@ -96,6 +100,8 @@ func (sjf *SJF) getProcess() int {
 }
 
 func (sjf *SJF) getAvgWaitingTIme() float32 {
+	fmt.Println("total waiting time = ", sjf.totalWaitingTime)
+	fmt.Println("total jobs = ", sjf.totalProcessesExecuted)
 	return float32(sjf.totalWaitingTime) / float32(sjf.totalProcessesExecuted)
 }
 
@@ -103,6 +109,3 @@ func (sjf *SJF) getProcessWaitingTime(processId int) int {
 	return sjf.processes[processId].waitingTime
 }
 
-// func Scheduling(stream){
-
-// }
--- sjf/sjf_test.go
+++ sjf/sjf_test.go
@@ -1,7 +1,8 @@
 package sjf
 
 import (
-	"testing"
+	"testing";
+	"math"
 )
 
 // func TestSJF(t *testing.T) {
@@ -9,30 +10,49 @@ func TestSJF(t *testing.T) {
 	sjf := NewSJF()
 
 	// testcase := []int{3, 2, -1, 5, 3, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1}
-	testcase := []int{5, 5, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+	// testcase := []int{5, 5, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+	// testcase := []int{5, -1, 3, 4, 7, -1}
+	
+	testcase := []int{16, 10, -1, 1} // avg waiting = 9.67
+	// testcase := []int{16, 10, -1, 1, -12, 23, -13, 4} // avg waiting = 12.6
 
-	for i := 0; i < len(testcase); i++ {
-		//i is arrival time, testcase[i] is bursttime
-		// completion time - arrival time
-		// startime - arrival time
-		if testcase[i] == -1 {
+	i := 0
+	for sjf.processId != -2 { // -2 indicates that there are no more proccesses left to run in queue
+		if i > len(testcase)-1 { // run one by one until we finish all remaining jobs
 			sjf.run(nil, i)
-		} else {
+		} else if testcase[i] < 0 { // run algorithm with no new job added to queue
+			num := testcase[i] 
+			for k := 0; k < int(math.Abs(float64(num))); k++ {
+				sjf.run(nil, i)
+				i += 1
+			}
+			i -= 1 // since there is an increment at the end
+		} else { // new process is to be added to queue
 			sjf.run(&Process{id: i, arrivalTime: i, burstTime: testcase[i]}, i)
 		}
-		t.Errorf("%d", sjf.getProcess())
-
+		t.Logf("%d", sjf.getProcess())
+		i += 1
 	}
 
-	for i := 0; i < len(testcase); i++ {
-		//i is arrival time, testcase[i] is bursttime
-		// completion time - arrival time
-		// startime - arrival time
-		if testcase[i] != -1 {
-			t.Errorf("%d, %d:", i, sjf.getProcessWaitingTime(i))
+	// for i := 0; i < len(testcase); i++ {
+	// 	//i is arrival time, testcase[i] is bursttime
+	// 	// completion time - arrival time
+	// 	// startime - arrival time
+	// 	if testcase[i] == -1 {
+	// 		sjf.run(nil, i)
+	// 	} else {
+	// 		sjf.run(&Process{id: i, arrivalTime: i, burstTime: testcase[i]}, i)
+	// 	}
+	// 	t.Logf("%d", sjf.getProcess())
+	// }
+	
+	// print waiting time for each process (waiting time saved in hashmap)
+	for i := 0; i < len(testcase); i++ { 
+		if testcase[i] > 0 {
+			t.Logf("id: %d, waitingT: %d", i, sjf.getProcessWaitingTime(i))
 		}
 
 	}
 
-	t.Errorf("avg waiting time: %f", sjf.getAvgWaitingTIme())
+	t.Logf("avg waiting time: %f", sjf.getAvgWaitingTIme())
 }
